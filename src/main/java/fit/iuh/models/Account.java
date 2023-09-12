f0d0dd46fb4dfc5d0c3a900f5a090838b50db27c
Phạm Hà Nam <pnam55588@gmail.com>
2023-09-12 14:59:08 +0700

    build models

--- src/main/java/fit/iuh/models/Account.java
+++ src/main/java/fit/iuh/models/Account.java
@@ -1,4 +1,81 @@
 package fit.iuh.models;
 
+import java.util.Objects;
+
 public class Account {
+//    account_id, full_name, password, email,
+//phone, status)
+    private String id;
+    private String fullName;
+    private String password;
+    private int status;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public Account(String id, String fullName, String password, int status) {
+        this.id = id;
+        this.fullName = fullName;
+        this.password = password;
+        this.status = status;
+    }
+
+    public Account() {
+    }
+
+    public Account(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Account account = (Account) o;
+        return Objects.equals(id, account.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "id='" + id + '\'' +
+                ", fullName='" + fullName + '\'' +
+                ", password='" + password + '\'' +
+                ", status=" + status +
+                '}';
+    }
 }
--- src/main/java/fit/iuh/models/GrantAccess.java
+++ src/main/java/fit/iuh/models/GrantAccess.java
@@ -1,4 +1,80 @@
 package fit.iuh.models;
 
+import java.util.Objects;
+
 public class GrantAccess {
+//    (account_id, role_id, is_grant, note
+    private  String accountId;
+    private String roleId;
+    private boolean isGrant;
+    private String note;
+
+    public GrantAccess(String accountId) {
+        this.accountId = accountId;
+    }
+
+    public GrantAccess() {
+    }
+
+    public GrantAccess(String accountId, String roleId, boolean isGrant, String note) {
+        this.accountId = accountId;
+        this.roleId = roleId;
+        this.isGrant = isGrant;
+        this.note = note;
+    }
+
+    public String getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(String accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(String roleId) {
+        this.roleId = roleId;
+    }
+
+    public boolean isGrant() {
+        return isGrant;
+    }
+
+    public void setGrant(boolean grant) {
+        isGrant = grant;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        GrantAccess that = (GrantAccess) o;
+        return Objects.equals(accountId, that.accountId) && Objects.equals(roleId, that.roleId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(accountId, roleId);
+    }
+
+    @Override
+    public String toString() {
+        return "GrantAccess{" +
+                "accountId='" + accountId + '\'' +
+                ", roleId='" + roleId + '\'' +
+                ", isGrant=" + isGrant +
+                ", note='" + note + '\'' +
+                '}';
+    }
 }
